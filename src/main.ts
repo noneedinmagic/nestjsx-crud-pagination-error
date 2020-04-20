ae52c08ce593eaf90cdc5057ca22f64fe508a626
Oleg <noneedinmagic@gmail.com>
2020-04-20 18:42:14 +0300

    Set up Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'module-alias/register';
 
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const apiPort = process.env.PORT || 8765;
 const apiBasePath = process.env.BASE_API_PATH || 'api';
@@ -10,6 +11,11 @@ import { AppModule } from '@app/AppModule';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(apiBasePath);
+
+  const doc = new DocumentBuilder()
+    // .setTitle()
+    .build();
+  SwaggerModule.setup(apiBasePath, app, SwaggerModule.createDocument(app, doc));
 
   await app.listen(apiPort);
 }
