bd7a205d82a204931a4534d1177c5725c65bf947
Oleg <noneedinmagic@gmail.com>
2020-04-20 19:52:54 +0300

    Use crud config service

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import 'module-alias/register';
 
 import { NestFactory } from '@nestjs/core';
+import { CrudConfigService } from '@nestjsx/crud';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const apiPort = process.env.PORT || 8765;
 const apiBasePath = process.env.BASE_API_PATH || 'api';
+
+CrudConfigService.load({
+  auth: {
+    property: 'user',
+  },
+  query: {
+    limit: 50,
+    maxLimit: 500,
+    alwaysPaginate: true,
+  },
+  routes: {
+    exclude: ['replaceOneBase'],
+    deleteOneBase: {
+      returnDeleted: true,
+    },
+  },
+});
 
 import { AppModule } from '@app/AppModule';
 
